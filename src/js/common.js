84a1b0bb3644c2aa5023ef96f1f093c046398bb2
Kyaw Thant Zin <52561987+kyaw-thant-zin@users.noreply.github.com>
2021-03-24 13:07:29 +0630

    Fixed loading JS

--- src/js/common.js
+++ src/js/common.js
@@ -1,10 +1,15 @@
-$(function() {
-  $(function() {
-    $('#page').css('display', 'none');
-    $('#loader').fadeIn(1000);
-  });
-  $(window).on('load', function() { //全ての読み込みが完了したら実行
-    $('#loader-bg').delay(3000).fadeOut(400);
+$(window).on('load', function () { //全ての読み込みが完了したら実行
+  $('#page').css('display', 'none');
+  $('#loader').fadeIn(1000);
+  $('body').addClass('no-scroll');
+});
+
+$(function () {
+  $(function () {
+    $('#loader-bg').delay(3000).fadeOut(400, function () {
+      $('body').removeClass('no-scroll');
+    });
     $('#page').slideUp(400).css('display', 'block');
   });
 });
+
